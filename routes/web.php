c6550e5d4d0878d0061044ff1ef345c0a9b2087d
adollking2 <brunoalex.id1990@gmail.com>
2023-01-24 20:27:24 +0700

    add bostrap

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', [
+        'title' => 'Home',
+        'name' => 'azzam',
+        'email' => "hore@email.com",
+    ]);
 });
+
+Route::get('/about', function () {
+    return view('about', [
+        'title' => 'About'
+    ]);
+});
+
+
+Route::get('/posts', function () {
+    return view('posts');
+});
+
