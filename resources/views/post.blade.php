4de58fadae6f1e8bb640db7d6860a415065de30d
adollking2 <brunoalex.id1990@gmail.com>
2023-01-24 21:51:35 +0700

    add single post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,11 +4,18 @@
 
 {{-- section--}}
 @section('container')
+<h1> {{ $title }}</h1>
+<h5>{{ $post['title'] }}</h5>
+<p>
+    {{ $post['body'] }}
+</p>
+{{-- 
   @foreach ($posts as $post)
   <article class="mb-5">
-    <h2><a href="http:/post/{{ $post["slug"]}}">{{ $post["title"] }}</a></h2>   
+    <h2><a href="http:/posts/{{ $post["slug"]}}">{{ $post["title"] }}</a></h2>   
     <h5> by : {{ $post["author"] }}</h5>
     <p>{{ $post["body"] }}</p>
   </article>
-  @endforeach  
+  @endforeach
+   --}}  
 @endsection
